714ef48cc6e030b45f7f169c794cfacfec2a0f61
rayray1996 <raytan96@gmail.com>
2022-10-07 02:02:37 +0800

    first commit

---
+++ EncoderDecoder.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class EncoderDecoder {
+
+    // initial list of mapping
+    public static String listOfChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*+,-./";
+
+    public EncoderDecoder() {
+    }
+
+    // encode string
+    public String encode(String plainText, String offsetChar) {
+        // to determine how much we need to downshift the list
+        int offsetVal = listOfChar.indexOf(offsetChar);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(offsetChar);
+
+        // to iterate through the plainText 1 by 1, and determining the final encoded
+        // string
+        for (int i = 0; i < plainText.length(); i++) {
+            String curr = String.valueOf(plainText.charAt(i));
+            if (curr.equals(" ")) {
+                sb.append(" ");
+                continue;
+            } else {
+                int idxOfCurr = listOfChar.indexOf(curr);
+                int pos = math(idxOfCurr, offsetVal);
+
+                String encodedChar = String.valueOf(listOfChar.charAt(pos));
+
+                sb.append(encodedChar);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // decode string
+    public String decode(String encodedText) {
+        StringBuilder sb = new StringBuilder();
+        String offsetChar = String.valueOf(encodedText.charAt(0));
+
+        // to determine how much we need to downshift the lift
+        int offsetVal = listOfChar.indexOf(offsetChar);
+
+        if (offsetVal < 0) {
+            return null;
+        } else {
+            // to iterate through the plainText 1 by 1, and determining the final decoded
+            // string
+            for (int i = 1; i < encodedText.length(); i++) {
+                String curr = String.valueOf(encodedText.charAt(i));
+
+                if (curr.equals(" ")) {
+                    sb.append(" ");
+                    continue;
+                } else {
+                    int idxOfCurr = listOfChar.indexOf(curr);
+                    sb.append(String.valueOf(listOfChar.charAt((idxOfCurr + offsetVal) % 44)));
+                }
+            }
+
+            return sb.toString();
+        }
+    }
+
+    // to calculate the offset position of a string to be encoded
+    // if the value of (i-offset) is negative, then the idx must continue from the
+    // bottom of the list
+    // else if (i-offset) is non negative, then we can simply return i-offset as the
+    // idx
+    private int math(int i, int offset) {
+        return ((i - offset) < 0) ? 44 + (i - offset) : i - offset;
+    }
+
+    // to check if a String of length 1 is a valid offset character
+    public static boolean validOffsetChar(String offsetChar) {
+        return (listOfChar.indexOf(offsetChar) >= 0 && offsetChar.length() == 1) ? true : false;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the offset character: ");
+        String offset = sc.nextLine();
+        // if user input for offset is not a valid offset char, then disallow the input
+        // and get them to re-enter an offset input that is valid
+        while (!EncoderDecoder.validOffsetChar(offset)) {
+            if (offset.length() > 1) {
+                System.out.println("Only 1 character can be used as the offset character.");
+            } else {
+                System.out.println("Offset character is invalid.");
+            }
+            System.out.println("Enter the offset character again: ");
+            offset = sc.nextLine();
+        }
+
+        System.out.println("Encode or Decode? (input only E or D): ");
+        String choice = sc.nextLine();
+        // if user choice is not D or E, then disallow the input and force them to
+        // re-enter a valid choice
+        while (!(choice.equals("D") || choice.equals("E"))) {
+            System.out.println("Please enter a valid input: ");
+            choice = sc.nextLine();
+        }
+
+        System.out.println("Enter the string to encode or decode: ");
+        String input = sc.nextLine();
+        // if a user choice is empty, null, not fully capitalized or the first character
+        // of the input decoded string is not a valid offset character then force them
+        // to re-enter a valid input
+        while (input == null ||
+                input.equals("") ||
+                !(input.toUpperCase().hashCode() == input.hashCode()) ||
+                (choice.equals("D") && !EncoderDecoder.validOffsetChar(String.valueOf(input.charAt(0))))) {
+            if (input == null || input.equals("")) {
+                System.out.println("Enter the string to encode or decode: ");
+            } else if (!(input.toUpperCase().hashCode() == input.hashCode())) {
+                System.out.println("Input string to encode or decode must be capitalized");
+                System.out.println("Enter the string to encode or decode: ");
+            } else {
+                System.out.println("Offset character of the encoded text is invalid");
+                System.out.println("Enter the string to encode or decode: ");
+            }
+            input = sc.nextLine();
+        }
+
+        EncoderDecoder encoderDecoder = new EncoderDecoder();
+
+        if (choice.equals("D")) {
+            System.out.println("Your decoded string is: " + encoderDecoder.decode(input));
+        } else {
+            System.out.println("Your encoded string is: " + encoderDecoder.encode(input, offset));
+        }
+    }
+
+}
